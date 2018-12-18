a1e44853710f2a33b70a680c827375edb44074cc
telule <manueltelule@gmail.com>
2018-12-18 11:44:19 -0600

    Ajustes Login Final Redireccionamiento a vistas

---
+++ ProyectoTopicos/ProyectoTopicos/Models/LoginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTopicos.Models
+{
+    public class LoginInfo
+    {
+        public string TipoCuenta { get; set; }
+        public string Contrasenna { get; set; }
+    }
+}
