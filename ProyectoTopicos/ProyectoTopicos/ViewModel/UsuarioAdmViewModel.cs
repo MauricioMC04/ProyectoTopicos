c30af118bd598ba8d105a23a0cdc0b7188f8e48f
Danny Alejandro Columna <dannycolumna19@yahoo.es>
2018-12-22 10:59:07 -0600

    Modificaciones en toda la interfaz

--- ProyectoTopicos/ProyectoTopicos/ViewModel/UsuarioAdmViewModel.cs
+++ ProyectoTopicos/ProyectoTopicos/ViewModel/UsuarioAdmViewModel.cs
@@ -17,6 +17,18 @@ namespace ProyectoTopicos.ViewModel
         //public string nombrePerfil { get; set; }
 
 
-        
+        public List<ArticulosDTO> ListaArticulos { get; set; }
+        public List<SelectListItem> listaCategorias { get; set; }
+        public List<SelectListItem> listaSubCategorias { get; set; }
+
+        public string codigoArticulo { get; set; }
+        public string nombre { get; set; }
+        public System.DateTime fechaIngreso { get; set; }
+        public string estado { get; set; }
+        public string categoria { get; set; }
+        public string subCategoria { get; set; }
+        public Nullable<System.DateTime> fechaEntrega { get; set; }
+        public string descripcion { get; set; }
+
     }
 }
